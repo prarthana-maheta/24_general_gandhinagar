6eeb7d320deab8bda91ff92980488996439b046e
prarthana-maheta <142563768+prarthana-maheta@users.noreply.github.com>
2024-05-17 20:01:39 +0530

    Add files via upload

---
+++ may_15.py
@@ -0,0 +1,29 @@
+# basic data types in python 
+
+# Integers 
+# a= 1212
+# intergers are not iterables
+
+# Float 
+# a =1.1234567
+
+# Char--String 
+# str1 = "wertyuiopoiugfdsxcvbnmnbvc nmbvdsyuilkjhgcc m 1234567890 !@#$%^&*()"
+
+# Complex -- 1j+1
+# ab="12"
+# a=str(ab)
+
+
+# String 
+# Immutable, Ordered
+
+str1 = "We are the student of python general batch at gandhinagar"
+print(len(str1))
+print(str1[-10])
+print(str1[40])
+print(str1[67])
+print(str1[:67:])
+# Index --- address
+# postive negative 
+
---
+++ may_8.py
@@ -0,0 +1,16 @@
+# print("lets start \n royal")
+
+# char name = "royal";
+
+name="royalqwertyuioihgfdsazxcvbnm,mnbvczxcvbnm,kjhgfds1234567890!@#$%^&*()_     dfjk."
+Name=1234
+# name=1
+# apple,banana,mango,a,g = "red","yellow","orange","1234"
+c=2
+a=1
+b=5
+a,b=b,a
+print(a,b)
+
+
+# print(apple,banana,mango)
